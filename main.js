a9ad8a24d0a5c0391d18fdbc1cb97d6b46e15943
ikrajzq <ivangoncharov52728@gmail.com>
2026-07-03 23:16:19 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+//1 номер
+const firstName = 'Ivan';
+const lastName = 'Petrov';
+let isStudent = true;
+
+//2 номер 
+let age = 20;
+let currentYear = 2026;
+let birthYear = currentYear - age;
+
+//3 номер 
+console.log("Меня зовут " + firstName + lastName + ",мне "+ age+ " лет. Я ученик курса: " + isStudent);
+
+//4 номер 
+//ответ: 123456789 - строка
